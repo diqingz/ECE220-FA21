0c8b7a75d62a088083c57541fe0d82537c6ac3d1
Diqing Zuo <diqingz2@illinois.edu>
2022-04-30 21:54:59 -0500

    mp12 finished

--- mp12/shape.hpp
+++ mp12/shape.hpp
@@ -7,9 +7,14 @@
 #include<algorithm>
 #include<list>
 #include<fstream>
-#include <iomanip>    
+#include <iomanip> 
 
 using namespace std;
+
+/*
+	Partners netID: staceyl2
+
+*/
 
 //Base class
 //Please implement Shape's member functions
@@ -19,11 +24,11 @@ public:
 	//Base class' constructor should be called in derived classes'
 	//constructor to initizlize Shape's private variable 
   	Shape(string name) {
-
+		  name_ = name;
 	}
 	
   	string getName() {
-
+		  return name_;
 	}
 	
   	virtual double getArea() const = 0;
@@ -43,31 +48,36 @@ template <class T>
 class Rectangle : public Shape{
 public:
   	Rectangle<T>(T width = 0, T length = 0):Shape("Rectangle") {
-
+		  width_ = width;
+		  length_ = length;
 	}
 	
   	double getArea() const {
-
+		  return (double) width_*length_;
 	}
 	
   	double getVolume() const {
-
+		  return 0;
 	}
 	
 	Rectangle<T> operator + (const Rectangle<T>& rec) {
-
+		double l = length_ + rec.getLength();
+		double w = width_ + rec.getWidth();
+		return Rectangle<T>(w,l);
 	}
 	
 	Rectangle<T> operator - (const Rectangle<T>& rec) {
-
+		double l = max((T)0, length_ - rec.getLength());
+		double w = max((T)0, width_ - rec.getWidth());
+		return Rectangle<T>(w,l);
 	} 
 	
 	T getWidth() const { 
-	
+		return width_;
 	}
 	
 	T getLength() const { 
-	
+		return length_;
 	}
 private:
 	T width_;
@@ -83,27 +93,29 @@ private:
 class Circle : public Shape{
 public:
   	Circle(double radius):Shape("Circle") {
-
+		radius_ = radius;
 	}
 	
   	double getArea() const{
-
+		return radius_*radius_*M_PI;
 	}
 	
  	double getVolume() const{
-
+		return 0;
 	}
 	
   	Circle operator + (const Circle& cir){
-
+		  double r = radius_ + cir.getRadius();
+		  return Circle(r);
 	}
 	
 	Circle operator - (const Circle& cir){
-
+		  double r = radius_ - cir.getRadius();
+		  return Circle(r);
 	} 
 
 	double getRadius() const { 
-	
+		return radius_;
 	}
 	
 private:
@@ -121,27 +133,29 @@ private:
 class Sphere : public Shape{
 public:
   	Sphere(double radius):Shape("Sphere") {
-
+		  radius_ = radius;
 	}
 
   	double getVolume() const {
-
+		  return (4.0/3.0)*radius_*radius_*radius_*M_PI;
 	}	
 	
   	double getArea() const {
-
+		  return 4*M_PI*radius_*radius_;
 	}
 
 	Sphere operator + (const Sphere& sph) {
-
+		  double r = radius_ + sph.getRadius();
+		  return Sphere(r);
 	}
 
 	Sphere operator - (const Sphere& sph) {
-
+		  double r = radius_ - sph.getRadius();
+		  return Sphere(r);
 	} 
 	
 	double getRadius() const {
-		
+		return radius_;
 	}
 
 private:
@@ -156,35 +170,43 @@ private:
 class RectPrism : public Shape{
 public:
   	RectPrism(double width, double length, double height):Shape("RectPrism") {
-
+		length_ = length;
+		width_ = width;
+		height_ = height;
 	}
 	
   	double getVolume() const {
-
+		  return length_*width_*height_;
 	}
   	
 	double getArea() const {
-
+		return 2*(length_*width_+length_*height_+width_*height_);
 	}
 	
 	RectPrism operator + (const RectPrism& rectp){
-
+		double l = length_ + rectp.getLength();
+		double w = width_ + rectp.getWidth();
+		double h = height_ + rectp.getHeight();
+		return RectPrism(w,l,h);
 	}
 	
 	RectPrism operator - (const RectPrism& rectp){
-
+		double l = max(0.0, length_ - rectp.getLength());
+		double w = max(0.0, width_ - rectp.getWidth());
+		double h = max(0.0, height_ - rectp.getHeight());
+		return RectPrism(w,l,h);
 	}	
 	
 	double getWidth() const { 
-	
+		return width_;
 	}
 	
 	double getLength() const { 
-	
+		return length_;
 	}
 	
 	double getHeight() const { 
-
+		return height_;
 	}
 private:
   	double length_;
@@ -197,17 +219,52 @@ private:
 // Return a vector of pointers that points to the objects 
 static list<Shape*> CreateShapes(char* file_name) {
 	//@@Insert your code here
-	
-	return list<Shape*>(0, NULL);;
+	int totalNum = 0;
+	ifstream myFile (file_name, ifstream::in);
+	myFile >> totalNum;
+	list<Shape*>myShape;
+	string shape = "";
+	getline(myFile, shape);
+	for(int i = 0; i < totalNum; i++) {
+		getline(myFile, shape, ' ');
+		if(shape.compare("Circle") == 0){
+			double r;
+			myFile >> r;
+			myShape.push_back(new Circle(r));
+			
+		} else if(shape.compare("Rectangle") == 0){
+			double w;
+			double l;
+			myFile >> w >> l;
+			myShape.push_back(new Rectangle<double>(w,l));
+			
+		} else if(shape.compare("Sphere") == 0){
+			double r;
+			myFile >> r;
+			myShape.push_back(new Sphere(r));
+
+		} else if(shape.compare("RectPrism") == 0){
+			double w,l,h;
+			myFile >> w >> l >> h;
+			myShape.push_back(new RectPrism(w,l,h));
+		}
+		getline(myFile, shape); // blank line
+	}
+	myFile.close();
+	return myShape;
 }
 
 // call getArea() of each object 
 // return the max area
 static double MaxArea(list<Shape*> shapes){
 	double max_area = 0;
+	list<Shape*>:: iterator it;
 	//@@Insert your code here
-
-	
+	for (it = shapes.begin(); it != shapes.end(); it++){
+		if (((*it)->getArea()) > max_area) {
+			max_area = (*it)->getArea();
+		}
+	}
 	return max_area;
 }
 
@@ -215,10 +272,13 @@ static double MaxArea(list<Shape*> shapes){
 // return the max volume
 static double MaxVolume(list<Shape*> shapes){
 	double max_volume = 0;
+	list<Shape*>:: iterator it;
 	//@@Insert your code here
-
-	
+	for (it = shapes.begin(); it != shapes.end(); it++){
+		if (((*it)->getVolume()) > max_volume) {
+			max_volume = (*it)->getVolume();
+		}
+	}
 	return max_volume;
 }
 #endif
-
